0a89580787672d96db2e3c037307d66586f6b7d3
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-08-12 16:13:03 +0530

    extract contents of 'div.quiz-content' to module 'QuizQuestionsView' as a component.

--- src/components/QuizContent.js
+++ src/components/QuizContent.js
@@ -1,53 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { QuizScorePage } from './QuizScorePage';
-import { QuizStartPage } from './QuizStartPage';
+import { QuizStartPage } from './QuizStartView';
+import { QuizQuestionsView } from './QuizQuestionsView';
+import { QuizScorePage } from './QuizScoreView';
 
-export const QuizContent = props =>
-
-  (props.quizState === 'progress' || props.quizState === 'check') ?
+export const QuizContent = ({ quizState, ...otherProps }) =>
 
   <div className="quiz-content">
-    <h3>{props.question}</h3>
-    <div className="quiz-question">
-      {props.choices.map((choice, index) => {
-        return (
-          <div 
-            className="quiz-choice" 
-            key={`q-${props.questionId}-i-${index}`}
-            onChange={props.handleChoiceClick}
-          >
-            <label>
-              <input 
-                type="radio" 
-                value={index + 1} 
-                name={`q-${props.questionId}-choice`}
-                data-questionid={props.questionId + 1}
-                defaultChecked={(index + 1) === Number(props.answers[props.questionId + 1])}
-              />
-              {choice}
-            </label>
-          </div>
-        );
-      })}
-    </div>
-  </div>
-
-  : (props.quizState === 'begin') ? <QuizStartPage content={props.intro}/> 
-  : (props.quizState === 'end') ? <QuizScorePage score={props.score} totalQuestions={props.totalQuestions}/>
+  {
+  (quizState === 'progress' || quizState === 'check') ?
+    <QuizQuestionsView {...otherProps}/>
+  : (quizState === 'begin') ? <QuizStartPage {...otherProps}/> 
+  : (quizState === 'end') ? <QuizScorePage {...otherProps}/>
   : null
-;
+  }
+  </div>;
 
 QuizContent.propTypes = {
 
   quizState: PropTypes.string.isRequired,
-  questionId: PropTypes.number.isRequired,
-  question: PropTypes.string.isRequired,
-  choices: PropTypes.array.isRequired,
-  answers: PropTypes.object.isRequired,
-  handleChoiceClick: PropTypes.func.isRequired,
   intro: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   totalQuestions: PropTypes.number.isRequired
 
 };
+
+
---
+++ src/components/QuizQuestionsView.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const QuizQuestionsView = props =>
+
+  <div className="quiz-questions-page">
+    <h3>{props.question}</h3>
+      <div className="quiz-question">
+        {props.choices.map((choice, index) => {
+          return (
+            <div 
+              className="quiz-choice" 
+              key={`q-${props.questionId}-i-${index}`}
+              onChange={props.handleChoiceClick}
+            >
+              <label>
+                <input 
+                  type="radio" 
+                  value={index + 1} 
+                  name={`q-${props.questionId}-choice`}
+                  data-questionid={props.questionId + 1}
+                  defaultChecked={(index + 1) === Number(props.answers[props.questionId + 1])}
+                />
+                {choice}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+  </div>
+
+;
+
+QuizQuestionsView.propTypes = {
+
+  questionId: PropTypes.number.isRequired,
+  question: PropTypes.string.isRequired,
+  choices: PropTypes.array.isRequired,
+  answers: PropTypes.object.isRequired,
+  handleChoiceClick: PropTypes.func.isRequired,
+
+};
